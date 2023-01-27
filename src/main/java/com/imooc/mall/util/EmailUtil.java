86dd02fbc0fcdc63deccc400caf487b9ad3cda06
15297718239 <58822089+15297718239@users.noreply.github.com>
2023-01-27 21:37:00 +0800

    imooc-mall商城后台
    imooc-mall商城后台

---
+++ src/main/java/com/imooc/mall/util/EmailUtil.java
@@ -0,0 +1,44 @@
+package com.imooc.mall.util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+/**
+ * 描述：     Email工具
+ */
+public class EmailUtil {
+
+    public static String genVerificationCode() {
+        List<String> verificationChars = Arrays.asList(
+                new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
+                        "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"});
+        Collections.shuffle(verificationChars);
+        //shuffle（list）方法用于通过使用默认随机性对指定的列表元素进行随机重新排序来工作。
+        String result = "";
+        for (int i = 0; i < 6; i++) {
+            result += verificationChars.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args)   {
+        System.out.println(EmailUtil.genVerificationCode());
+    }
+
+    public static boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress internetAddress = new InternetAddress(email);
+            internetAddress.validate();
+        } catch (AddressException e) {
+            e.printStackTrace();
+            result = false;
+        }
+        return result;
+    }
+
+
+}
---
+++ src/test/java/com/imooc/mall/MallApplicationTests.java
@@ -0,0 +1,14 @@
+package com.imooc.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("aaa");
+    }
+
+}
